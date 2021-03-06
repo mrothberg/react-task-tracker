e602bed45dae63f7ada989ed1dd62c55f997b3e5
Michael Rothberg <michaelwrothberg@gmail.com>
2021-03-06 16:50:00 -0500

    Add task form 1:48:47

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -22,7 +23,13 @@ function App() {
         day: 'Feb 5th at 2:30 pm',
         reminder: false,
     }
-  ]) 
+  ])
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -47,6 +54,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasksComponent}
     </div>
   );
