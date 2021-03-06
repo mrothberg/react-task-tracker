42602ea08ec10b61293502b49a5fc74f40b6ec7d
Michael Rothberg <michaelwrothberg@gmail.com>
2021-03-06 02:54:05 -0500

    Task components 51:56 in video

---
+++ src/components/Tasks.js
@@ -0,0 +1,14 @@
+import Task from './Task'
+
+const Tasks = ({ tasks }) => {
+    return (
+        // setTasks([...tasks, {}])
+        <>
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} />
+            ))}
+        </>
+    )
+}
+
+export default Tasks
